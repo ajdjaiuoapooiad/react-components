12cd57ff60dd76af8bd576779be8d2a93a8c1fd5
user <user@usernoMacBook-Air.local>
2024-09-06 10:02:06 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <Header /> 
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
