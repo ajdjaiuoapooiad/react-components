8b300a645df4b1203eb9a91b5baa9146b234a745
user <user@usernoMacBook-Air.local>
2024-09-06 10:49:40 +0900

    coreComponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import Header from './components/Header';
+import ComponentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
+
+function CoreConcept(props){
+  return(
+    <li>
+      <img src={props.img} alt="..." />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 
 function App() {
@@ -8,7 +20,31 @@ function App() {
       
       <Header /> 
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts" >
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            img={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept 
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            img={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept 
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            img={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept 
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            img={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
